cfac3701a4a88c26754bc3ae9d1efb7849b9e4fa
KwagHyeyeong <89637930+nbje1007@users.noreply.github.com>
2021-12-04 20:17:46 +0900

    제주수목원 2차업로드
    제주수목원 2차업로드

--- js/script.js
+++ js/script.js
@@ -6,4 +6,20 @@ $(document).ready(function(){
     		scrollTop: 0
     	});
     });
+
+    // 탭메뉴
+    var notice_menu_a = $('.notice-menu a');
+    var notice_list= $('.notice-list');
+    $.each(notice_menu_a, function(index, item){
+        notice_menu_a.eq(index).click(function(e){
+            // a 태그라서
+            e.preventDefault();
+            notice_menu_a.removeClass('notice-menu-active');
+            notice_menu_a.eq(index).addClass('notice-menu-active');
+
+            notice_list.removeClass('notice-list-active');
+            notice_list.eq(index).addClass('notice-list-active');
+            
+        })
+    });
 });
